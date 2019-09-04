75372d60c00406c02ce75be86ed64745470b2b6e
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-09-04 10:36:14 +0530

    Create disable.py

---
+++ actions/disable.py
@@ -0,0 +1,18 @@
+---
+name: disable
+runner_type: python-script
+description: Enable monitoring on a node
+enabled: true
+entry_point: run.py
+parameters:
+  action:
+    type: string
+    immutable: true
+    default: ex_disable
+  region:
+    type: string
+    description: The region code
+    default: dd-na
+  node_id:
+    type: string
+    description: The ID of the node (VM)
